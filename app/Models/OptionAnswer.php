55c418eb8fc58053359264712c84ba09b144ddbe
Chanheng-Ngen <ngen.chanheng.2823@rupp.edu.kh>
2026-04-26 22:31:31 +0700

    init schema of question questiontype question optionanswer model

---
+++ app/Models/OptionAnswer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OptionAnswer extends Model
+{
+    protected $fillable = [
+        'content',
+        'is_correct',
+        'question_id',
+    ];
+}
