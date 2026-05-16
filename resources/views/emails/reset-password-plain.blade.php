7c07b272a6703ee332eb357214619a2e1748ac6e
Chanheng-Ngen <ngen.chanheng.2823@rupp.edu.kh>
2026-05-16 13:27:55 +0700

    custom mail template

--- resources/views/emails/reset-password-plain.blade.php
+++
@@ -1,6 +0,0 @@
-Hi {{ $user->name }},
-
-Click the link below to reset your password:
-{{ url('/api/auth/reset-password?token=' . $token . '&email=' . $user->email) }}
-
-If you didn't request this, ignore this email.
